3ebb5d017d2e2e22278e6c1b93bab4ed4c02affb
parvezaman <emailtoamanullah@gmail.com>
2021-10-18 09:42:40 +0600

    hosted to firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDi-clnfKkgw7YT0srCgG4uIdY68Al1g2U",
+    authDomain: "kushtia-healthcare-center.firebaseapp.com",
+    projectId: "kushtia-healthcare-center",
+    storageBucket: "kushtia-healthcare-center.appspot.com",
+    messagingSenderId: "679374999964",
+    appId: "1:679374999964:web:650006a8b364edcb633859"
+  };
+
+  export default firebaseConfig;
