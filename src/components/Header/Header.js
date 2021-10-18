6427903648374438072d4440b193ee1220ccb8ad
parvezaman <emailtoamanullah@gmail.com>
2021-10-18 16:10:22 +0600

    header format done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Header.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link, NavLink } from 'react-router-dom';
 
@@ -7,11 +8,14 @@ const Header = () => {
         <div>
             <Navbar bg="dark" variant="dark">
     <Container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Navbar.Brand href="/home">KushtiaHealthCare</Navbar.Brand>
     <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+
+      <Link className="me-3 text-decoration-none text-white" to="/home">Home</Link>
+      <Link className="me-3 text-decoration-none text-white" to="/services">Services</Link>
+      <Link className="me-3 text-decoration-none text-white" to="/doctors">Doctors</Link>
+      <Link className="me-3 text-decoration-none text-white" to="/appointments">Appointments</Link>
+      <Link className="me-3 text-decoration-none text-white" to="/login">Login</Link>
     </Nav>
     </Container>
   </Navbar>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,19 +6,7 @@ const Home = () => {
     return (
         <div>
             <h1>Home</h1>
-            <div class="btn-group" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-primary">Left</button>
-            <button type="button" class="btn btn-primary">Middle</button>
-            <button type="button" class="btn btn-primary">Right</button>
-            </div>
-
-
-
-            <ButtonGroup aria-label="Basic example">
-  <Button variant="secondary">Left</Button>
-  <Button variant="secondary">Middle</Button>
-  <Button variant="secondary">Right</Button>
-</ButtonGroup>
+            
         </div>
     );
 };
